3a3a7097e1b6cce2cbcae52329ab21a404f1daac
Ziv Ismailov <zivismailov@gmail.com>
2024-11-06 13:28:31 +0200

    Extending the main.tf with SG

--- main.tf
+++ main.tf
@@ -20,5 +20,58 @@ resource "aws_instance" "netflix_app" {
 
   tags = {
     Name = "netflix-app"
+    Env = "dev"
   }
+
+  resource "aws_security_group" "netflix_app_sg" {
+  name        = "ziv-netflix-app-sg"   # change <your-name> accordingly
+  description = "Allow SSH and HTTP traffic"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+  resource "aws_security_group" "netflix_app_sg" {
+  name        = "ziv-netflix-app-sg"   # change <your-name> accordingly
+  description = "Allow SSH and HTTP traffic"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 }
